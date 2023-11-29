9bb550b186f807b76e92ccefce9fbba459d6a26a
Alex Cardoso <alex.cfreire@al.infnet.edu.br>
2023-11-29 14:24:20 -0300

    Atualizando classe Servico

--- src/main/java/br/com/limpezaJob/Models/Servico.java
+++ src/main/java/br/com/limpezaJob/Models/Servico.java
@@ -1,7 +1,43 @@
 package br.com.limpezaJob.Models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servico {
     private Imovel imovel;
     private Funcionario funcionario;
+    private  Fatura fatura;
+    private LocalDateTime dataHoraCotacao;
+    private double valor;
+    private LocalDateTime dataHoraExecucao;
+    private List<Funcionario> equipeFaxineiros = new ArrayList<Funcionario>();
+    private double avaliacao;
+
+    public Servico(Imovel imovel, Funcionario funcionario, Fatura fatura, LocalDateTime dataHoraCotacao, double valor, LocalDateTime dataHoraExecucao) {
+        this.imovel = imovel;
+        this.funcionario = funcionario;
+        this.fatura = fatura;
+        this.dataHoraCotacao = dataHoraCotacao;
+        this.valor = valor;
+        this.dataHoraExecucao = dataHoraExecucao;
+    }
+
+    public void marcarVisitaParaCotacao(LocalDateTime data, Imovel imovel, Funcionario gerente){
+
+    }
+
+    public boolean marcarRealizacaoDoServico(){
+        return false;
+    }
+
+    public boolean adicionarMembroNaEquipe(Funcionario faxineiro){
+        equipeFaxineiros.add(faxineiro);
+        return true;
+    }
+
+    public void avaliarServico(double nota){
+        this.avaliacao = nota;
+    }
 
 }
